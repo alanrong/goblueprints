229dfe6877d3afb4dc3206920f8113622c7a0edc
yuntifree <yuntifree@qq.com>
2017-05-10 21:22:10 +0800

    add websocket

--- chat/main.go
+++ chat/main.go
@@ -6,6 +6,8 @@ import (
 	"net/http"
 	"path/filepath"
 	"sync"
+
+	"github.com/gorilla/websocket"
 )
 
 type templateHandler struct {
@@ -22,8 +24,38 @@ func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	t.templ.Execute(w, nil)
 }
 
+type client struct {
+	socket *websocket.Conn
+	send   chan []byte
+	room   *room
+}
+
+func (c *client) read() {
+	defer c.socket.Close()
+	for {
+		_, msg, err := c.socket.ReadMessage()
+		if err != nil {
+			return
+		}
+		c.room.forward <- msg
+	}
+}
+
+func (c *client) write() {
+	defer c.socket.Close()
+	for msg := range c.send {
+		err := c.socket.WriteMessage(websocket.TextMessage, msg)
+		if err != nil {
+			return
+		}
+	}
+}
+
 func main() {
+	r := newRoom()
 	http.Handle("/", &templateHandler{filename: "chat.html"})
+	http.Handle("/room", r)
+	go r.run()
 	//start server
 	if err := http.ListenAndServe(":9090", nil); err != nil {
 		log.Fatal("ListenAndServe:", err)
