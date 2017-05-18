3f5a9b32f447e9570227df39e9fd50f3f4b07a20
yuntifree <yuntifree@qq.com>
2017-05-18 15:33:17 +0800

    fix import path

--- chat/main.go
+++ chat/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"flag"
-	"goblueprints/trace"
 	"html/template"
 	"log"
 	"net/http"
@@ -10,6 +9,7 @@ import (
 	"path/filepath"
 	"sync"
 
+	"github.com/alanrong/goblueprints/trace"
 	"github.com/gorilla/websocket"
 )
 
